f38f85398842f56469dc2f79d22a46428213f3cf
bendazoo <abendezu385@gmail.com>
2024-03-11 22:43:51 -0400

    Added str_len method

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <strings.h>
+
+int str_len(char *word[15]);
 
 int main() {
-    printf("Hello, World!\n");
+
+    char *word[15];
+
+    printf("Give me a word: ");
+    scanf("%p", word);
+
+    printf("%d",str_len(word));
+
+
     return 0;
+}
+
+int str_len(char *word[15]){
+
+    int counter = 0;
+    for (int i = 0; i <= sizeof(*word); i++){
+        counter++;
+    }
+    return counter;
 }
